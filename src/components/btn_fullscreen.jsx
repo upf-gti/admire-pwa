3bcf9e1aec8d7d4abd6bb41e01e1cf72e5e4378c
h3R <hermann.plass@gmail.com>
2021-12-05 14:07:08 +0100

    fullscreen button fix

--- src/components/btn_fullscreen.jsx
+++ src/components/btn_fullscreen.jsx
@@ -4,19 +4,31 @@ import { useState, useEffect } from 'react'
 
 
 export default () => {  
-    let [state, setState] = useState(0);//eslint-disable-line
+    let [state, setState] = useState(0);
+
+    function handleClick(){
+        /*let promise
+        if (document.fullscreenElement) {
+            promise = document.exitFullscreen();
+        } else {
+            promise = document.documentElement.requestFullscreen();
+        }
+        promise.then( ()=>setState(++state) );*/
+        screenfull.toggle();
+    }
 
     useEffect(()=>{
-        const f = () => setState(s => ++s);
+        const f = () => setState(++state);
         screenfull.on('change', f);
     return ()=>{
         screenfull.off('change', f);
-    }},[])
+    }
+    },[])
     
     return <div id="btn_fullscreen">
         { screenfull.isEnabled && 
             <Button 
-            onClick={screenfull.toggle} variant="link"> 
+            onClick={handleClick} variant="link"> 
                 {  document.fullscreen && <i className="bi bi-fullscreen-exit"/> }
                 { !document.fullscreen && <i className="bi bi-fullscreen"/> }
             </Button>    
