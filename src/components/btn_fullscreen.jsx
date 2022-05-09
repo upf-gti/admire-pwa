e693fb737850f0c024fd3358ef36ab071707b7b5
jxarco <alexroco.30@gmail.com>
2022-05-09 16:46:43 +0200

    rework fullscren btn (ios bug)

--- src/components/btn_fullscreen.jsx
+++ src/components/btn_fullscreen.jsx
@@ -1,36 +1,24 @@
 import screenfull from 'screenfull'
 import { Button } from 'react-bootstrap'
-import { useState, useEffect } from 'react'
-
 
 export default () => {  
-    let [state, setState] = useState(0);
 
     function handleClick(){
-        /*let promise
-        if (document.fullscreenElement) {
-            promise = document.exitFullscreen();
-        } else {
-            promise = document.documentElement.requestFullscreen();
-        }
-        promise.then( ()=>setState(++state) );*/
         screenfull.toggle();
+        document.inFullscreen = !document.inFullscreen;
+
+        const fsButton = document.getElementById('FS-Button');
+        if(fsButton) {
+            const icon = fsButton.children[0];
+            icon.className = document.inFullscreen ? "bi bi-fullscreen-exit" : "bi bi-fullscreen";
+        }
     }
 
-    useEffect(()=>{
-        const f = () => setState(++state);
-        screenfull.on('change', f);
-    return ()=>{
-        screenfull.off('change', f);
-    }
-    },[])
-    
     return <div id="btn_fullscreen">
         { screenfull.isEnabled && 
-            <Button 
+            <Button id="FS-Button"
             onClick={handleClick} variant="link"> 
-                {  document.fullscreen && <i className="bi bi-fullscreen-exit"/> }
-                { !document.fullscreen && <i className="bi bi-fullscreen"/> }
+            {  <i className="bi bi-fullscreen"/> }
             </Button>    
         }
     </div>;
